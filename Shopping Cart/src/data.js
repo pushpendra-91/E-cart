4cf04573ea41574563384090ce769d3178dbac9c
Pushpendra <pkanaujiya91@gmail.com>
2022-09-12 13:47:10 +0530

    update

--- Shopping Cart/src/data.js
+++ Shopping Cart/src/data.js
@@ -3,26 +3,55 @@ let shopItemsData = [{
     name: "Casual Shirt",
     price: 450,
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, cum",
-    img: "https://images.unsplash.com/photo-1579783900882-c0d3dad7b119?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=60"
+    img: "https://images.unsplash.com/photo-1596755094514-f87e34085b2c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80"
 },
 {
     id:"second",
     name: "Formal Shirt",
     price: 400,
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, cum",
-    img: "https://images.unsplash.com/photo-1579783900882-c0d3dad7b119?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=60"
+    img: "https://images.unsplash.com/photo-1602810320073-1230c46d89d4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
 },
 {
     id:"third",
     name: "T-Shirt",
     price: 300,
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, cum",
-    img: "https://images.unsplash.com/photo-1579783900882-c0d3dad7b119?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=60"
+    img: "https://images.unsplash.com/photo-1583743814966-8936f5b7be1a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
 },
 {
     id:"fourth",
     name: "Jeans",
     price: 600,
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, cum",
-    img: "https://images.unsplash.com/photo-1579783900882-c0d3dad7b119?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=60"
-}]
+    img: "https://images.unsplash.com/photo-1584370848010-d7fe6bc767ec?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+},
+{
+    id: "fifth",
+    name: "Shoes",
+    price: "800",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, cum",
+    img: "https://images.unsplash.com/photo-1588099768531-a72d4a198538?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+},
+{
+    id: "sixth",
+    name: "Suit",
+    price: "3250",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, cum",
+    img: "https://images.unsplash.com/photo-1534030347209-467a5b0ad3e6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+},
+{
+    id: "seventh",
+    name: "Dress",
+    price: "2250",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, cum",
+    img: "https://images.unsplash.com/photo-1605763240000-7e93b172d754?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+},
+{
+    id: "eight",
+    name: "Jacket",
+    price: "1850",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, cum",
+    img: "https://images.unsplash.com/photo-1544022613-e87ca75a784a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+}
+]
